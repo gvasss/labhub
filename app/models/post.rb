c3eb3d19f7e443efddfd65bfeb1035e121f37fb3
Angelos Sakellariou <aggelossakell@gmail.com>
2025-02-06 19:35:03 +0200

    Add specs for the Post model's validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,24 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    belongs_to :category
+  belongs_to :user
+  belongs_to :category
 
-    default_scope -> { includes(:user).order(created_at: :desc) }
+  default_scope -> { includes(:user).order(created_at: :desc) }
 
-    scope :by_category, -> (branch, category_name) do 
-      joins(:category).where(categories: {name: category_name, branch: branch}) 
-    end
-    
-    scope :by_branch, -> (branch) do
-      joins(:category).where(categories: {branch: branch}) 
-    end
-    
-    scope :search, -> (search) do
-      #where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
-      where("LOWER(title) LIKE :search OR LOWER(content) LIKE :search", search: "%#{search.downcase}%")
-    end
+  scope :by_category, -> (branch, category_name) do 
+    joins(:category).where(categories: {name: category_name, branch: branch}) 
+  end
+  
+  scope :by_branch, -> (branch) do
+    joins(:category).where(categories: {branch: branch}) 
+  end
+  
+  scope :search, -> (search) do
+    #where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
+    where("LOWER(title) LIKE :search OR LOWER(content) LIKE :search", search: "%#{search.downcase}%")
+  end
 
-    validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
   validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
   validates :category_id, presence: true
+  validates :user_id, presence: true
 end
