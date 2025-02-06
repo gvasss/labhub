69587d9280936746951ed5650a610ba84e3e3944
Angelos Sakellariou <aggelossakell@gmail.com>
2025-02-06 19:27:45 +0200

    Add validations to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,9 @@ class Post < ApplicationRecord
     scope :search, -> (search) do
       #where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
       where("LOWER(title) LIKE :search OR LOWER(content) LIKE :search", search: "%#{search.downcase}%")
-
     end
+
+    validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :category_id, presence: true
 end
