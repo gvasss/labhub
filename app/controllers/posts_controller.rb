2ed84c167555935a846ec0e0c888fd42fd891897
Angelos Sakellariou <aggelossakell@gmail.com>
2025-02-06 19:00:15 +0200

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,38 +1,60 @@
 class PostsController < ApplicationController
-    def show
-      @post = Post.find(params[:id])
-    end    
+  before_action :redirect_if_not_signed_in, only: [:new]
 
-    def hobby
-      posts_for_branch(params[:action])
-    end
-    
-    def study
-      posts_for_branch(params[:action])
-    end
-    
-    def team
-      posts_for_branch(params[:action])
-    end
-    
-    private
+  def show
+    @post = Post.find(params[:id])
+  end    
 
-    def posts_for_branch(branch)
-      @categories = Category.where(branch: branch)
-      @posts = get_posts.paginate(page: params[:page])
+  def hobby
+    posts_for_branch(params[:action])
+  end
+  
+  def study
+    posts_for_branch(params[:action])
+  end
+  
+  def team
+    posts_for_branch(params[:action])
+  end
 
-      respond_to do |format|
-        format.html
-        format.js { render partial: 'posts/posts_pagination_page' }
-      end
-    end
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
 
-    def get_posts
-      PostsForBranchService.new({
-        search: params[:search],
-        category: params[:category],
-        branch: params[:action]
-      }).call
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
     end
+  end
+  
+  private
+
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = get_posts.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'posts/posts_pagination_page' }
+    end
+  end
+
+  def get_posts
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
+  end
 
 end
