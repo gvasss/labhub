a41e931885f445697909bba7956ea022e3c1f72a
Gerasimos Vasileiou <gerasimosvass@gmail.com.com>
2025-02-06 13:24:23 +0200

    Fix posts template

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import "bootstrap";
-
+ 
 export default class extends Controller {
   connect() {
     document.body.addEventListener("click", (event) => {
@@ -9,21 +9,21 @@ export default class extends Controller {
       }
     });
   }
-
+ 
   showModal(event) {
     const post = event.target.closest(".single-post-card, .single-post-list");
     if (!post) return;
-
+ 
+    const postId = post.dataset.id;
     const postedBy = post.querySelector(".post-content .posted-by")?.innerHTML;
     const postHeading = post.querySelector(".post-content h3")?.innerHTML;
     const postContent = post.querySelector(".post-content p")?.innerHTML;
-    const interestedLink = post.querySelector(".post-content .interested a")?.getAttribute("href");
-
+ 
     document.querySelector("#postModal .posted-by").textContent = postedBy;
     document.querySelector("#postModal .loaded-data h3").textContent = postHeading;
     document.querySelector("#postModal .loaded-data p").textContent = postContent;
-    document.querySelector("#postModal .loaded-data .interested a").setAttribute("href", interestedLink);
-
+    document.querySelector("#postModal .loaded-data .interested a").setAttribute("href", `/posts/${postId}`);
+ 
     const modal = new bootstrap.Modal(document.getElementById("postModal"));
     modal.show();
   }
